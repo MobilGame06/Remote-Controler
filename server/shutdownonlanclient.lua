6f4b8c1c17dd47d4f624605b8cf2af054f89268a
Mobil Game 06 <75528709+MobilGame06@users.noreply.github.com>
2021-08-06 16:59:14 +0200

    Update shutdownonlanclient.lua

--- server/shutdownonlanclient.lua
+++ server/shutdownonlanclient.lua
@@ -11,7 +11,7 @@ modem.open(port)
 
 while true do
 local type, _, _, _, _, message = event.pull(2, "modem")
-if type == "modem_message" and message == "stop1" then
+if type == "modem_message" and message == "stop2" then
  computer.shutdown()
 end
 end
